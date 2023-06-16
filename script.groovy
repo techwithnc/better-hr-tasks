c510a8a6c204d39dc52eacc26769a2385dbf8e7f
TechWithNC <techwithnc.techwithnc@gmail.com>
2023-06-16 12:36:53 +0630

    add deploy docker image step

--- script.groovy
+++ script.groovy
@@ -8,22 +8,22 @@ def pushImage(){
                         sh "docker push techwithnc/betterhrapp:$APP_VERSION"
                     }
 }
-def deployImage(){
-    withCredentials([sshUserPrivateKey(credentialsId: 'svrssh', keyFileVariable: 'SSH_KEY_FILE', passphraseVariable: '', usernameVariable: 'SSH_USERNAME')]) {
-                    sshagent(['SSH_KEY_FILE']) {
-                        sshCommand remote: "ssh -o StrictHostKeyChecking=no $SSH_USERNAME@3.96.169.134",
-                            command:'''
-                                printenv
-                            '''       
-                    }
-                }
-}
-
 // def deployImage(){
-//     // def dockerRun = 'sudo docker image pull techwithnc/betterhrapp:5.0'
-//     sshagent(['awssvrssh']){
-//        sh "ssh -o StrictHostKeyChecking=no ubuntu@3.96.169.134 echo"
-//     }
+//     withCredentials([sshUserPrivateKey(credentialsId: 'svrssh', keyFileVariable: 'SSH_KEY_FILE', passphraseVariable: '', usernameVariable: 'SSH_USERNAME')]) {
+//                     sshagent(['SSH_KEY_FILE']) {
+//                         sshCommand remote: "ssh -o StrictHostKeyChecking=no $SSH_USERNAME@3.96.169.134",
+//                             command:'''
+//                                 printenv
+//                             '''       
+//                     }
+//                 }
 // }
+
+def deployImage(){
+    // def dockerRun = 'sudo docker image pull techwithnc/betterhrapp:5.0'
+    sshagent(['svrssh']){
+       sh "ssh -o StrictHostKeyChecking=no ubuntu@3.96.169.134 echo"
+    }
+}
 // 
 return this
