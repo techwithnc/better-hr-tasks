2ecf731374df2bcea9f31bcd9c350db4f53abbac
TechWithNC <techwithnc.techwithnc@gmail.com>
2023-06-16 12:13:55 +0630

    add deploy docker image step

--- script.groovy
+++ script.groovy
@@ -21,8 +21,8 @@ def pushImage(){
 
 def deployImage(){
     // def dockerRun = 'sudo docker image pull techwithnc/betterhrapp:5.0'
-    sshagent(['svr01-ssh']){
-       sh 'ssh -o StrictHostKeyChecking=no ubuntu@3.96.169.134 echo'
+    sshagent(['awssvrssh']){
+       sh "ssh -o StrictHostKeyChecking=no ubuntu@3.96.169.134 echo"
     }
 }
 return this
