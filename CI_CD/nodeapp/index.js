5a8999345e5fda92cc8af3a3247e66fe7e98c654
TechWithNC <techwithnc.techwithnc@gmail.com>
2023-06-17 08:31:17 +0630

    change app version to 801.0

--- CI_CD/nodeapp/index.js
+++ CI_CD/nodeapp/index.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World,version 701.0');
+  res.send('Hello Better HR, I am Aung Nyein Chan Oo,this is version 801.0');
 });
 
 app.listen(PORT, HOST, () => {
